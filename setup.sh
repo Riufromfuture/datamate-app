19dd052fa4643bf38b8be70b00975acf44a6894f
Ankur Raj <142142430+Riufromfuture@users.noreply.github.com>
2025-05-08 01:57:22 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,5 @@
 pip install --upgrade pip
 
-pip install paddlepaddle==2.6.1 -f https://www.paddlepaddle.org.cn/whl/paddle_cpu.html
-
 # Install Tesseract
 apt-get update
 apt-get install -y tesseract-ocr
